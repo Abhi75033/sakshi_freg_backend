f068acc29bece025bb61ad1c2cacd207f7efb416
Abhishek Kumar <abhishekyadav1112.21@gmail.com>
2026-03-09 13:29:42 +0530

    fix: add localhost:5174 to CORS whitelist for admin panel

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(cors({
     origin: [
         'https://saksahi-freg-frontend.vercel.app',
         'http://localhost:5173',
+        'http://localhost:5174',
         'http://localhost:8080',
     ],
     credentials: true,
